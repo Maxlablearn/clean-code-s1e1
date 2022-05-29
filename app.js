df4c11f6757ce7eb4d0ebb2612c46fe4fa25bc8d
MaxLabLearn <maxlablearn@gmail.com>
2022-05-29 19:58:21 +0300

    fix: add alt text for images according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = 'image';
+    deleteButtonImg.alt = 'Delete';
     deleteButton.appendChild(deleteButtonImg);
 
 
